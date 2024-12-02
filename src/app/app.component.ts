b59b41482b7d2ae7e49fa4f12960130669b83b35
rv-2002 <raguvaranraguvaran020@gmail.com>
2024-12-02 10:05:48 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ViewComponent } from './view/view.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeComponent,ViewComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  host: {ngSkipHydration: 'true'}
 })
 export class AppComponent {
   title = 'proj';
